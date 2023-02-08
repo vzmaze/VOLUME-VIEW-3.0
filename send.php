c1774c01da4728bd6c7df7f3eb43626b7698a581
vzmaze <63446855+vzmaze@users.noreply.github.com>
2023-02-08 14:10:24 +0200

    Удалил закомментированную часть, где создавалась страница-уведовление

--- send.php
+++ send.php
@@ -34,19 +34,3 @@ $send = mail ($myadress,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nF
 ini_set('short_open_tag', 'On');
 header('Refresh: 3; URL=index.html');
 ?>
-<!-- <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="refresh" content="3; url=index.html">
-<title>Спасибо! Мы свяжемся с вами!</title>
-<meta name="generator">
-<script type="text/javascript">
-setTimeout('location.replace("/index.html")', 3000);
-/*Изменить текущий адрес страницы через 3 секунды (3000 миллисекунд)*/
-</script> 
-</head>
-<body>
-<h1>Спасибо! Мы свяжемся с вами!</h1>
-</body>
-</html> -->
